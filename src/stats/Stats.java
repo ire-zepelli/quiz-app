2759abc7027524df460b700cde38fa1cdc0b9b16
Benedict Avenido <benedictavenido1320@gmail.com>
2024-09-09 20:41:24 +0800

    [small fix] revert score back to zero

--- src/stats/Stats.java
+++ src/stats/Stats.java
@@ -1,5 +1,5 @@
 package stats;
 
 public class Stats {
-   public static int math=10, english=10, it=10, history=10, science=10, pop=10,custom=10; 
+   public static int math, english, it, history, science, pop,custom; 
 }
